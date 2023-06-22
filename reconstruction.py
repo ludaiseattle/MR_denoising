73a2c399035ba8ab7e60c99925c1bef76de57982
LU DAI <alyld7@sting.cs.nott.ac.uk>
2023-06-22 23:10:41 +0100

    change unsampling and rebuild logic, output looks normal

--- reconstruction.py
+++ reconstruction.py
@@ -1,4 +1,6 @@
+#import cupy as np
 import numpy as np
+#cp.cuda.Device().use()
 
 def reconstruct(kspace_data, autocalibration_data, acceleration_factor):
     autocalibration_size = autocalibration_data.shape[0]
@@ -7,21 +9,43 @@ def reconstruct(kspace_data, autocalibration_data, acceleration_factor):
         for j in range(autocalibration_size):
                 print(autocalibration_data[i, j])
     """
-    #half_size = autocalibration_size // 2
-    half_size = 8 
+    #threshold = autocalibration_size // 2
+    threshold = 8 
+
+    top = 0
+    bottom = 0
+    left = 0
+    right = 0
+    size = kspace_data.shape[0]
     
     reconstructed_kspace = np.zeros(kspace_data.shape, dtype=np.complex128)
     for row in range(0, kspace_data.shape[0]):
         for col in range(0, kspace_data.shape[1]):
-            if (row < half_size or row >= kspace_data.shape[0]-half_size or col < half_size or col >= kspace_data.shape[1]-half_size):
-                reconstructed_kspace[row, col] = autocalibration_data[row, col] 
+            if row < threshold:
+                top = 0
             else:
-                if kspace_data[row, col] == 0:
-                    reconstructed_kspace[row, col] = np.real(np.average(autocalibration_data[row-half_size:row+half_size, col-half_size:col+half_size])) + np.imag(autocalibration_data[row, col])
-                    #reconstructed_kspace[row, col] = np.real(reconstructed_value).astype(np.uint) 
-                    #print("recon: ", reconstructed_kspace[row, col])
-                else:
-                    reconstructed_kspace[row, col] = kspace_data[row, col] 
-                    #print("origin: ", reconstructed_kspace[row, col])
+                top = row - threshold 
+
+            if row > size - threshold:
+                bottom = size
+            else:
+                bottom = row + threshold
+
+            if col < threshold:
+                left = 0
+            else:
+                left = col - threshold
+
+            if col > size - threshold:
+                right = size
+            else:
+                right = col + threshold
+
+            if kspace_data[row, col] == 0:
+                reconstructed_kspace[row, col] = np.real(np.average(autocalibration_data[top:bottom, left:right])) + np.imag(autocalibration_data[row, col])
+                #print("recon: ", reconstructed_kspace[row, col])
+            else:
+                reconstructed_kspace[row, col] = kspace_data[row, col] 
+                #print("origin: ", reconstructed_kspace[row, col])
     return reconstructed_kspace
 
--- undersampling.py
+++ undersampling.py
@@ -1,17 +1,26 @@
 import cv2
 import numpy as np
 
+def is_central(cthred, x, y, width, height):
+    if ((x > width//2 - cthred and x < width//2 + cthred) 
+    and (y > height//2 - cthred and y < height//2 + cthred)):
+        return True
+    else:
+        return False
+
 def samp(fft):
     height, width = fft.shape[:2]
 
     image1 = np.zeros((height, width), dtype=np.complex128)
     image2 = np.zeros((height, width), dtype=np.complex128)
 
+    cthred = (width//2)*(8/10)
+
     for x in range(width):
         for y in range(height):
-            if x % 2 == 0:
+            if x % 2 == 0 or is_central(cthred, x, y, width, height):
                 image1[x, y] = fft[x, y]
-            else:
+            if x % 2 == 1 or is_central(cthred, x, y, width, height):
                 image2[x, y] = fft[x, y]
 
     return image1, image2
